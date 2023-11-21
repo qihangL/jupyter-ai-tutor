8d0aee524e2dd547d107a5ce3fb7be61e781c4f6
qihangL <liangqihang1@hotmail.com>
2023-11-21 08:10:46 +0800

    minor ui improve

--- src/markdown.tsx
+++ src/markdown.tsx
@@ -5,7 +5,6 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import React from 'react';
 
-
 type MarkdownRendererProps = {
   children: string;
 };
@@ -15,15 +14,31 @@ export function MarkdownRenderer({ children: markdown }: MarkdownRendererProps) 
     <Markdown
       remarkPlugins={[remarkGfm]}
       components={{
-        code({ node, inline, className, children, ...props }: any) {
+        code(props) {
+          const { children, className, node, ...rest } = props;
           const match = /language-(\w+)/.exec(className || '');
-
-          return !inline && match ? (
-            <SyntaxHighlighter style={oneDark} PreTag="div" language={match[1]} {...props}>
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              PreTag="div"
+              children={String(children).replace(/\n$/, '')}
+              language={match[1]}
+              style={oneDark}
+              ref={React.createRef<SyntaxHighlighter>()}
+            />
           ) : (
-            <code className={className} {...props}>
+            <code
+              {...rest}
+              className={className}
+              style={{
+                backgroundColor: '#f5f5f5', // Slightly lighter gray, similar to GitHub's style
+                fontFamily: 'SFMono-Regular, Consolas, Liberation Mono, Menlo, monospace', // Monospaced font stack
+                fontSize: '90%', // Slightly smaller font size for a more compact look
+                padding: '3px 6px', // Adjusted padding for better visual balance
+                borderRadius: '6px', // Slightly larger border-radius for a smoother look
+                border: '1px solid #d1d5da' // Subtle border similar to GitHub's style
+            }}
+            >
               {children}
             </code>
           );
